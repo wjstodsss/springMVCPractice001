c0d85116abf692aed3ee077675ed2cd1609712cd
wjstodsss <wjstodsss@naver.com>
2024-02-20 10:48:52 +0900

    local java program - YoilTeller

---
+++ src/main/java/com/project/sample/YoilTeller.java
@@ -0,0 +1,29 @@
+package com.project.sample;
+
+import java.util.Calendar;
+
+public class YoilTeller {
+	
+	public static void main(String[] args) {
+		// 1. 입력
+		String year = args[0];
+		String month = args[1];
+		String day = args[2];
+		
+		int yyyy = Integer.parseInt(year);
+		int mm = Integer.parseInt(month);
+		int dd = Integer.parseInt(day);
+		
+		// 2. 작업
+		Calendar cal = Calendar.getInstance();
+		cal.set(yyyy, mm - 1, dd);
+		
+		int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK);
+		char yoil = " 일월화수목금토".charAt(dayOfWeek);
+		
+		// 3. 출력
+		System.out.println(year + "년 " + month + "월 " + day + "일은 ");
+		System.out.println(yoil + "요일입니다.");
+	}
+
+}
