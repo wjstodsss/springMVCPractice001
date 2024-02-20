48687b97126f4f2e2d2a319648662c9aa53c15df
wjstodsss <wjstodsss@naver.com>
2024-02-20 11:02:09 +0900

    YoilTeller html태그를 활용하여 출력하도록 수정

--- src/main/java/com/project/sample/YoilTeller.java
+++ src/main/java/com/project/sample/YoilTeller.java
@@ -35,8 +35,14 @@ public class YoilTeller {
 		response.setContentType("text/html");
 		response.setCharacterEncoding("utf-8");
 		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("<head>");
+		out.println("</head>");
+		out.println("<body>");
 		out.println(year + "년 " + month + "월 " + day + "일은 ");
 		out.println(yoil + "요일입니다.");
+		out.println("</body>");
+		out.println("</html>");
 	}
 
 }
