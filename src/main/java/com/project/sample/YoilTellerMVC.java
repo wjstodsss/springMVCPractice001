fc6fa029aa453f1d9aa85c09342ceaf7d9d3bcf9
wjstodsss <wjstodsss@naver.com>
2024-02-21 13:21:14 +0900

    출력분리, 유효성검사작성, 입력이 바른경우, 그렇지 않은경우 페이지 작성

--- src/main/java/com/project/sample/YoilTellerMVC.java
+++ src/main/java/com/project/sample/YoilTellerMVC.java
@@ -1,34 +1,42 @@
 package com.project.sample;
 
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.time.DateTimeException;
+import java.time.LocalDate;
 import java.util.Calendar;
 
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller // 원격 프로그램 등록 
 public class YoilTellerMVC {
 	@RequestMapping("/getYoilMVC") // url과 메서드 연결
-	public void main(int year, int month, int day, HttpServletResponse response) throws IOException {
+	public String main(int year, int month, int day, Model model) throws IOException {
+		
+		if (!isValid(year, month, day)) {
+			return "yoilError";
+		}
+		
 		
 		char yoil = getYoil(year, month, day);
 		
-		response.setContentType("text/html");
-		response.setCharacterEncoding("utf-8");
-		PrintWriter out = response.getWriter();
+		model.addAttribute("year", year);
+		model.addAttribute("month", month);
+		model.addAttribute("day", day);
+		model.addAttribute("yoil", yoil);
 		
-		out.println("<html>");
-		out.println("<head>");
-		out.println("</head>");
-		out.println("<body>");
-		out.println(year + "년 " + month + "월 " + day + "일은 ");
-		out.println(yoil + "요일입니다.");
-		out.println("</body>");
-		out.println("</html>");
-		
+		return "yoil";
+
+	}
+
+	private boolean isValid(int year, int month, int day) {
+		try {
+			LocalDate.of(year, month, day);
+			return true;
+		} catch (DateTimeException e)  {
+			return false;
+		}
 	}
 
 	private char getYoil(int year, int month, int day) {
